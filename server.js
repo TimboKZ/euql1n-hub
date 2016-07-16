9a05f8bf7dbe6380ee4460dcce1f7f910ef30499
TimboKZ <tima_kuzh@inbox.ru>
2016-07-16 12:05:39 +0100

    Create NPM project, setup ExpressJS Hello World

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Main server file for euql1n Hub
+ *
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ * @version 0.0.1
+ */
+
+/**
+ * Config section
+ */
+var SERVER_PORT = 8080;
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(SERVER_PORT, function () {
+    console.log('Example app listening on port 3000!');
+});
